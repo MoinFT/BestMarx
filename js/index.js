50b2deedd9e2a3d525d16fb13cf255bcbbf137f5
MoinFT <fabian.tonne@outlook.de>
2021-10-01 21:41:34 +0200

    Added home page and navigation

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function showNavigation() {
+    let navigation = document.getElementById("navigation")
+    let navigationIcon = document.getElementById("navigationIcon")
+
+    if (navigation.classList.contains("show")){
+        navigation.classList.remove("show")
+        navigationIcon.classList.remove("opened")
+
+    } else {
+        navigation.classList.add("show")
+        navigationIcon.classList.add("opened")
+    }
+}
