8cf3fd1f378ebccf6f665dd978f33bc9050e2cd7
MoinFT <fabian.tonne@outlook.de>
2021-10-10 02:28:46 +0200

    Added new Features - Added explanation texts for all laws - Added evaluation for distributiveLaw

--- js/distributiveLawExercise.js
+++ js/distributiveLawExercise.js
@@ -11,6 +11,8 @@ function startExercise(difficulty) {
 
     window.sessionStorage.setItem("exercise", JSON.stringify(exercise))
     setProgress(exercise)
+
+    createDistributivLawExercise()
 }
 
 function backToNavigation() {
@@ -19,4 +21,202 @@ function backToNavigation() {
 
     let lawExercise = document.getElementById("distributiveLawExercise")
     lawExercise.style.display = "none"
+}
+
+function createDistributivLawExercise() {
+    let exercise = createExerciseObject(window.sessionStorage.getItem("exercise"))
+    let exerciseDifficulty = exercise.difficulty
+
+    let div = document.getElementById("exercise")
+
+    let firstNumber
+    let secondNumber
+    let thirdNumber
+
+    if (exercise.exercises[exercise.exerciseNumber - 1] === undefined) {
+        firstNumber = Math.floor(Math.random() * 20 + 5)
+        secondNumber = Math.floor(Math.random() * 20 + 5)
+
+        while (secondNumber === firstNumber) {
+            secondNumber = Math.floor(Math.random() * 20 + 5)
+        }
+
+        thirdNumber = Math.floor(Math.random() * 20 + 5)
+
+        while (thirdNumber === firstNumber || thirdNumber === secondNumber) {
+            thirdNumber = Math.floor(Math.random() * 20 + 5)
+        }
+
+        let exerciseData = createExerciseDataObject("{}")
+        exerciseData.exerciseNumber = exercise.exerciseNumber
+        exerciseData.firstNumber = firstNumber
+        exerciseData.secondNumber = secondNumber
+        exerciseData.thirdNumber = thirdNumber
+
+        exercise.exercises.push(exerciseData)
+        window.sessionStorage.setItem("exercise", JSON.stringify(exercise))
+    } else {
+        firstNumber = exercise.exercises[exercise.exerciseNumber - 1].firstNumber
+        secondNumber = exercise.exercises[exercise.exerciseNumber - 1].secondNumber
+        thirdNumber = exercise.exercises[exercise.exerciseNumber - 1].thirdNumber
+    }
+
+    console.log(firstNumber)
+    console.log(secondNumber)
+    console.log(thirdNumber)
+
+    let html = ""
+
+    html += "<div>"
+    if (exerciseDifficulty === "light") {
+        html += `<p>${firstNumber} ⋅ (${secondNumber} + ${thirdNumber}) = ${firstNumber} ⋅ ${secondNumber} + </p>`
+        html += `<input oninput="getNumber(0)" class='textField' type='text' id='firstNumberAnswer' maxlength='2'/>`
+        html += `<p>⋅</p>`
+        html += `<input oninput="getNumber(1)" class='textField' type='text' id='secondNumberAnswer' maxlength='2'/>`
+    } else if (exerciseDifficulty === "medium") {
+        html += `<p>${firstNumber} ⋅ (${secondNumber} + ${thirdNumber}) = </p>`
+        html += `<input oninput="getNumber(0)" class='textField' type='text' id='firstNumberAnswer' maxlength='2'/>`
+        html += `<p>⋅</p>`
+        html += `<input oninput="getNumber(1)" class='textField' type='text' id='secondNumberAnswer' maxlength='2'/>`
+        html += `<p>+</p>`
+        html += `<input oninput="getNumber(2)" class='textField' type='text' id='thirdNumberAnswer' maxlength='2'/>`
+        html += `<p>⋅</p>`
+        html += `<input oninput="getNumber(3)" class='textField' type='text' id='fourthNumberAnswer' maxlength='2'/>`
+    } else if (exerciseDifficulty === "hard") {
+        html += `<input oninput="getNumber(0)" class='textField' type='text' id='firstNumberAnswer' maxlength='2'/>`
+        html += `<p>⋅ (</p>`
+        html += `<input oninput="getNumber(1)" class='textField' type='text' id='secondNumberAnswer' maxlength='2'/>`
+        html += `<p>+</p>`
+        html += `<input oninput="getNumber(2)" class='textField' type='text' id='thirdNumberAnswer' maxlength='2'/>`
+        html += `<p>) =</p>`
+        html += `<input oninput="getNumber(3)" class='textField' type='text' id='fourthNumberAnswer' maxlength='2'/>`
+        html += `<p>⋅</p>`
+        html += `<input oninput="getNumber(4)" class='textField' type='text' id='fifthNumberAnswer' maxlength='2'/>`
+        html += `<p>+</p>`
+        html += `<input oninput="getNumber(5)" class='textField' type='text' id='sixthNumberAnswer' maxlength='2'/>`
+        html += `<p>⋅</p>`
+        html += `<input oninput="getNumber(6)" class='textField' type='text' id='seventhNumberAnswer' maxlength='2'/>`
+    }
+    html += "</div>"
+
+    div.innerHTML = html
+}
+
+function finishExercise() {
+    let exercise = createExerciseObject(window.sessionStorage.getItem("exercise"))
+    let exerciseDifficulty = exercise.difficulty
+
+    let html = ""
+    let exerciseData = exercise.exercises
+
+    for (let i = 0; i < exercise.maxExerciseNumber; i++) {
+        let firstNumberAnswer = "0"
+        let secondNumberAnswer = "0"
+        let thirdNumberAnswer = "0"
+        let fourthNumberAnswer = "0"
+        let fifthNumberAnswer = "0"
+        let sixthNumberAnswer = "0"
+        let seventhNumberAnswer = "0"
+
+        for (let iAnswer = 0; iAnswer < exerciseData[i].answerNumbers.length; iAnswer++) {
+            if (exerciseData[i].answerNumbers[iAnswer].index === 0) {
+                firstNumberAnswer = exerciseData[i].answerNumbers[iAnswer].input
+            }
+
+            if (exerciseData[i].answerNumbers[iAnswer].index === 1) {
+                secondNumberAnswer = exerciseData[i].answerNumbers[iAnswer].input
+            }
+
+            if (exerciseData[i].answerNumbers[iAnswer].index === 2) {
+                thirdNumberAnswer = exerciseData[i].answerNumbers[iAnswer].input
+            }
+
+            if (exerciseData[i].answerNumbers[iAnswer].index === 3) {
+                fourthNumberAnswer = exerciseData[i].answerNumbers[iAnswer].input
+            }
+
+            if (exerciseData[i].answerNumbers[iAnswer].index === 4) {
+                fifthNumberAnswer = exerciseData[i].answerNumbers[iAnswer].input
+            }
+
+            if (exerciseData[i].answerNumbers[iAnswer].index === 5) {
+                sixthNumberAnswer = exerciseData[i].answerNumbers[iAnswer].input
+            }
+
+            if (exerciseData[i].answerNumbers[iAnswer].index === 6) {
+                seventhNumberAnswer = exerciseData[i].answerNumbers[iAnswer].input
+            }
+        }
+
+        if (exerciseDifficulty === "light") {
+            if (firstNumberAnswer !== secondNumberAnswer) {
+                if ((exerciseData[i].firstNumber.toString() === firstNumberAnswer || exerciseData[i].firstNumber.toString() === secondNumberAnswer)) {
+                    if ((exerciseData[i].thirdNumber.toString() === firstNumberAnswer || exerciseData[i].thirdNumber.toString() === secondNumberAnswer)) {
+                        html += `<p> ${i}: Richtig</p>`
+                    } else {
+                        html += `<p> ${i}: Teilweise Falsch</p>`
+                    }
+                } else {
+                    html += `<p> ${i}: Falsch</p>`
+                }
+            } else {
+                html += `<p> ${i}: Falsch</p>`
+            }
+        } else if (exerciseDifficulty === "medium") {
+            if (firstNumberAnswer !== secondNumberAnswer && thirdNumberAnswer !== fourthNumberAnswer) {
+                if ((exerciseData[i].firstNumber.toString() === firstNumberAnswer || exerciseData[i].firstNumber.toString() === secondNumberAnswer) && (exerciseData[i].firstNumber.toString() === thirdNumberAnswer || exerciseData[i].firstNumber.toString() === fourthNumberAnswer)) {
+                    if (exerciseData[i].secondNumber.toString() === firstNumberAnswer || exerciseData[i].secondNumber.toString() === secondNumberAnswer || exerciseData[i].secondNumber.toString() === thirdNumberAnswer || exerciseData[i].secondNumber.toString() === fourthNumberAnswer) {
+                        if (exerciseData[i].thirdNumber.toString() === firstNumberAnswer || exerciseData[i].thirdNumber.toString() === secondNumberAnswer || exerciseData[i].thirdNumber.toString() === thirdNumberAnswer || exerciseData[i].thirdNumber.toString() === fourthNumberAnswer) {
+                            html += `<p> ${i}: Richtig</p>`
+                        } else {
+                            html += `<p> ${i}: Teilweise Falsch</p>`
+                        }
+                    } else {
+                        html += `<p> ${i}: Teilweise Falsch</p>`
+                    }
+                } else {
+                    html += `<p> ${i}: Falsch</p>`
+                }
+            } else {
+                html += `<p> ${i}: Falsch</p>`
+            }
+        } else if (exerciseDifficulty === "hard") {
+            if (firstNumberAnswer !== secondNumberAnswer && firstNumberAnswer !== thirdNumberAnswer && secondNumberAnswer !== thirdNumberAnswer) {
+                if (exerciseData[i].firstNumber.toString() === firstNumberAnswer) {
+                    if (exerciseData[i].secondNumber.toString() === secondNumberAnswer || exerciseData[i].secondNumber.toString() === thirdNumberAnswer) {
+                        if (exerciseData[i].thirdNumber.toString() === secondNumberAnswer || exerciseData[i].thirdNumber.toString() === thirdNumberAnswer) {
+                            if (fourthNumberAnswer !== fifthNumberAnswer && sixthNumberAnswer !== seventhNumberAnswer) {
+                                if ((exerciseData[i].firstNumber.toString() === fourthNumberAnswer || exerciseData[i].firstNumber.toString() === fifthNumberAnswer) && (exerciseData[i].firstNumber.toString() === sixthNumberAnswer || exerciseData[i].firstNumber.toString() === seventhNumberAnswer)) {
+                                    if (exerciseData[i].secondNumber.toString() === fourthNumberAnswer || exerciseData[i].secondNumber.toString() === fifthNumberAnswer || exerciseData[i].secondNumber.toString() === sixthNumberAnswer || exerciseData[i].secondNumber.toString() === seventhNumberAnswer) {
+                                        if (exerciseData[i].thirdNumber.toString() === fourthNumberAnswer || exerciseData[i].thirdNumber.toString() === fifthNumberAnswer || exerciseData[i].thirdNumber.toString() === sixthNumberAnswer || exerciseData[i].thirdNumber.toString() === seventhNumberAnswer) {
+                                            html += `<p> ${i}: Richtig</p>`
+                                        } else {
+                                            html += `<p> ${i}: Teilweise Falsch</p>`
+                                        }
+                                    } else {
+                                        html += `<p> ${i}: Teilweise Falsch</p>`
+                                    }
+                                } else {
+                                    html += `<p> ${i}: Teilweise Falsch</p>`
+                                }
+                            } else {
+                                html += `<p> ${i}: Teilweise Falsch</p>`
+                            }
+                        } else {
+                            html += `<p> ${i}: Teilweise Falsch</p>`
+                        }
+                    } else {
+                        html += `<p> ${i}: Teilweise Falsch</p>`
+                    }
+                } else {
+                    html += `<p> ${i}: Falsch</p>`
+                }
+            } else {
+                html += `<p> ${i}: Falsch</p>`
+            }
+        }
+    }
+
+    let div = document.getElementById("exercise")
+    div.innerHTML = html
 }
--- js/exercise.js
+++ js/exercise.js
@@ -9,16 +9,7 @@ function setProgress(exercise) {
     exerciseRestBarElement.style.width = ((1 - (exercise.exerciseNumber / exercise.maxExerciseNumber)) * 100).toString() + "%"
 
     let forwardExerciseNavigation = document.getElementById("forwardExerciseNavigation")
-    let backExerciseNavigation = document.getElementById("backExerciseNavigation")
     let finishExerciseNavigation = document.getElementById("finishExerciseNavigation")
-
-    if (exercise.exerciseNumber === 0) {
-        backExerciseNavigation.style.display = "none"
-        exerciseRestBarElement.style.borderRadius = "0.5rem"
-    } else {
-        backExerciseNavigation.style.display = "inherit"
-        exerciseRestBarElement.style.borderRadius = "0 0.5rem 0.5rem 0"
-    }
 
     if (exercise.exerciseNumber === exercise.maxExerciseNumber) {
         forwardExerciseNavigation.style.display = "none"
@@ -34,13 +25,19 @@ function setProgress(exercise) {
 function backExerciseNavigation() {
     let exercise = createExerciseObject(window.sessionStorage.getItem("exercise"))
 
-    if (exercise.exerciseNumber > 0) {
+    if (exercise.exerciseNumber > 1) {
         exercise.exerciseNumber -= 1
 
-        setProgress(exercise)
-    }
+        window.sessionStorage.setItem("exercise", JSON.stringify(exercise))
 
-    window.sessionStorage.setItem("exercise", JSON.stringify(exercise))
+        setProgress(exercise)
+
+        switch (exercise.type) {
+            case "distributiveLaw":
+                createDistributivLawExercise()
+                break
+        }
+    }
 }
 
 function forwardExerciseNavigation() {
@@ -49,14 +46,65 @@ function forwardExerciseNavigation() {
     if (exercise.exerciseNumber < exercise.maxExerciseNumber) {
         exercise.exerciseNumber += 1
 
-        setProgress(exercise)
-    }
+        window.sessionStorage.setItem("exercise", JSON.stringify(exercise))
 
-    window.sessionStorage.setItem("exercise", JSON.stringify(exercise))
+        setProgress(exercise)
+
+        switch (exercise.type) {
+            case "distributiveLaw":
+                createDistributivLawExercise()
+                break
+        }
+    }
 }
 
-function finishExerciseNavigation() {
+function getNumber(temp) {
+    let exercise = createExerciseObject(window.sessionStorage.getItem("exercise"))
+    let exerciseData = exercise.exercises[exercise.exerciseNumber - 1]
 
+    let inputElementID = ""
+    switch (temp) {
+        case 0:
+            inputElementID = "firstNumberAnswer"
+            break
+        case 1:
+            inputElementID = "secondNumberAnswer"
+            break
+        case 2:
+            inputElementID = "thirdNumberAnswer"
+            break
+        case 3:
+            inputElementID = "fourthNumberAnswer"
+            break
+        case 4:
+            inputElementID = "fifthNumberAnswer"
+            break
+        case 5:
+            inputElementID = "sixthNumberAnswer"
+            break
+        case 6:
+            inputElementID = "seventhNumberAnswer"
+            break
+    }
+
+    let indexExist = false
+
+    for(let i = 0; i < exerciseData.answerNumbers.length; i++) {
+        if (exerciseData.answerNumbers[i].index === temp) {
+            exerciseData.answerNumbers[i].input = document.getElementById(inputElementID).value
+            indexExist = true
+        }
+    }
+
+    if (!indexExist) {
+        let inputField = createAnswerFieldObject("{}")
+        inputField.index = temp;
+        inputField.input = document.getElementById(inputElementID).value
+        exerciseData.answerNumbers.push(inputField)
+    }
+    exercise.exercises[exercise.exerciseNumber - 1] = exerciseData
+
+    window.sessionStorage.setItem("exercise", JSON.stringify(exercise))
 }
 
 function createExerciseObject(exerciseData) {
@@ -64,12 +112,42 @@ function createExerciseObject(exerciseData) {
 
     exerciseObject = Object.assign(
         {
-            exerciseNumber: 0,
+            exerciseNumber: 1,
             maxExerciseNumber: 10,
             difficulty: "",
-            type: ""
+            type: "",
+            exercises: []
         },
         exerciseObject
     )
     return exerciseObject
+}
+
+function createExerciseDataObject(exerciseData) {
+    let exerciseObject = JSON.parse(exerciseData)
+
+    exerciseObject = Object.assign(
+        {
+            exerciseNumber: 0,
+            firstNumber: 0,
+            secondNumber: 0,
+            thirdNumber: 0,
+            answerNumbers: [],
+        },
+        exerciseObject
+    )
+    return exerciseObject
+}
+
+function createAnswerFieldObject(fieldData) {
+    let fieldObject = JSON.parse(fieldData)
+
+    fieldObject = Object.assign(
+        {
+            index: 0,
+            input: 0,
+        },
+        fieldObject
+    )
+    return fieldObject;
 }
